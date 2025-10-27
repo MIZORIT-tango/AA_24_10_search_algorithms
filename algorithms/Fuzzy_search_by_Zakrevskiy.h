8c898d746cbdd370d69d0bdf6a82ab82d0ae69cb
KirillZazog <127005365+KirillZazog@users.noreply.github.com>
2025-10-28 00:57:16 +0300

    Изменена структура файла с пространства имен на структуру

--- algorithms/Fuzzy_search_by_Zakrevskiy.h
+++ algorithms/Fuzzy_search_by_Zakrevskiy.h
@@ -1,83 +1,87 @@
 #pragma once
-#include <string>
+#include "../search_race.h"
+#include "../utils.h"
 #include <vector>
+#include <string>
 #include <algorithm>
 #include <climits>
 
-namespace fuzzy_search {
-    int levenshtein_distance(const std::string& str1, const std::string& str2) {
-        int m = str1.length();
-        int n = str2.length();
+inline int levenshtein_distance(const std::string& str1, const std::string& str2) {
+    int m = str1.length();
+    int n = str2.length();
 
-        std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1));
-        for (int i = 0; i <= m; i++) {
-            dp[i][0] = i;
-        }
-        for (int j = 0; j <= n; j++) {
-            dp[0][j] = j;
-        }
+    std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1));
+    for (int i = 0; i <= m; i++) {
+        dp[i][0] = i;
+    }
+    for (int j = 0; j <= n; j++) {
+        dp[0][j] = j;
+    }
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (str1[i - 1] == str2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                }
-                else {
-                    dp[i][j] = 1 + std::min({ dp[i - 1][j],
-                                            dp[i][j - 1],
-                                            dp[i - 1][j - 1] });
-                }
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (str1[i - 1] == str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+            else {
+                dp[i][j] = 1 + std::min({ dp[i - 1][j],
+                                        dp[i][j - 1],
+                                        dp[i - 1][j - 1] });
             }
         }
-
-        return dp[m][n];
     }
 
-    struct SearchResult {
-        std::string word;
-        int distance;
-        size_t index;
+    return dp[m][n];
+}
 
-        SearchResult(const std::string& w, int d, size_t idx = 0)
-            : word(w), distance(d), index(idx) {
+SearchResult fuzzy_levenshtein(const std::string& file_path,
+    const std::string& target_word) {
+
+    SearchResult result;
+    result.found = false;
+    result.indices.clear();
+
+    std::string text = utils::readFile(file_path);
+    if (text.empty()) {
+        return result;
+    }
+
+    std::vector<std::string> words;
+    std::string current_word;
+
+    for (char c : text) {
+        if (std::isalpha(c) || std::isdigit(c)) {
+            current_word += c;
         }
-    };
-
-    std::vector<SearchResult> fuzzy_search(
-        const std::string& query,
-        const std::vector<std::string>& dictionary,
-        int max_distance = 3) {
-
-        std::vector<SearchResult> results;
-
-        for (size_t i = 0; i < dictionary.size(); i++) {
-            const auto& word = dictionary[i];
-            int distance = levenshtein_distance(query, word);
-
-            if (distance <= max_distance) {
-                results.emplace_back(word, distance, i);
+        else {
+            if (!current_word.empty()) {
+                words.push_back(current_word);
+                current_word.clear();
             }
         }
-        std::sort(results.begin(), results.end(),
-            [](const SearchResult& a, const SearchResult& b) {
-                return a.distance < b.distance;
-            });
-
-        return results;
     }
 
-    SearchResult find_best_match(
-        const std::string& query,
-        const std::vector<std::string>& dictionary,
-        int max_distance = 3) {
+    if (!current_word.empty()) {
+        words.push_back(current_word);
+    }
 
-        auto results = fuzzy_search(query, dictionary, max_distance);
+    int min_distance = INT_MAX;
+    std::string best_match;
+    size_t best_index = 0;
 
-        if (!results.empty()) {
-            return results[0];
+    for (size_t i = 0; i < words.size(); i++) {
+        int distance = levenshtein_distance(target_word, words[i]);
+        if (distance < min_distance) {
+            min_distance = distance;
+            best_match = words[i];
+            best_index = i;
         }
-
-        return SearchResult("", INT_MAX);
     }
 
+    if (min_distance <= 3) { // схожесть по умолчанию 3 символа
+        result.found = true;
+        result.indices.push_back(best_index);
+    }
+
+    return result;
 }
